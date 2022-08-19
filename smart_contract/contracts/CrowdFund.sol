b01fe7cc841b18e66a7bdb4b7bb4fc70e1e32d18
b_kris <bryankristofer25@gmail.com>
2022-08-20 02:04:28 +0800

    Add withdraw method; isActive and goalReached states and state changes

--- smart_contract/contracts/CrowdFund.sol
+++ smart_contract/contracts/CrowdFund.sol
@@ -6,6 +6,8 @@ contract CrowdFund {
     address payable public owner;
     string public metadataURI;
     uint public goal;
+    bool public isActive = true;
+    bool public goalReached = false;
 
     event Fund(address indexed _donor, uint _value);
 
@@ -24,5 +26,14 @@ contract CrowdFund {
     receive() external payable {
         funds[msg.sender] += msg.value;
         emit Fund(msg.sender, msg.value);
+        if (address(this).balance >= (goal * 1 ether)) {
+            goalReached = true;
+        }
+    }
+
+    function withdraw(uint _value) public payable {
+        (bool sent, bytes memory data) = owner.call{value: _value * 1 ether}("");
+        require(sent, "Error sending eth.");
+        isActive = false;
     }
 }
