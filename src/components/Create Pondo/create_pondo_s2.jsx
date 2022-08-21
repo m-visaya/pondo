c69ccbbb6bd19e19731d84a2f1283efa0d673262
Brierith <utilbrit.afforestation@slmail.me>
2022-08-21 09:28:11 +0800

    frontend tweaks

--- src/components/Create Pondo/create_pondo_s2.jsx
+++ src/components/Create Pondo/create_pondo_s2.jsx
@@ -5,7 +5,7 @@ export default function CreatePondoS1(){
     return(
         <div className="grid grid-cols-3 max-w-screen-xl mx-auto">
         <div className="col-span-1">
-            <div className="min-h-screen min-w-screen bg-pondo-blue-secondary">
+            <div className="min-h-screen min-w-screen bg-pondo-blue">
                 <div className="flex justify-center mx-auto">
                  <p className="text-[28pt] pt-36 mx-16 text-white">Tell the people more about your Pondo.</p>
                 </div>
